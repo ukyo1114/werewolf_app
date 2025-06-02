c0f6bc522342adba697d9b925c1322db982e7561
ukyo1114 <xjr1300sunny@gmail.com>
2025-06-02 17:18:58 +0900

    名前空間のハンドラーを設定

--- src/socketHandlers/socketHandler.ts
+++ src/socketHandlers/socketHandler.ts
@@ -1,6 +1,8 @@
 import { Server } from 'http';
 import { Server as SocketIOServer } from 'socket.io';
 import { entryNameSpaceHandler } from './entryNameSpace';
+import { chatNameSpaceHandler } from './chatNameSpace';
+import { gameNameSpaceHandler } from './gameNameSpace';
 
 export const socketHandler = (server: Server): SocketIOServer => {
   const io = new SocketIOServer(server, {
@@ -12,6 +14,8 @@ export const socketHandler = (server: Server): SocketIOServer => {
   });
   // 名前空間のハンドラーを設定
   entryNameSpaceHandler(io.of('/entry'));
+  chatNameSpaceHandler(io.of('/chat'));
+  gameNameSpaceHandler(io.of('/game'));
 
   return io;
 };
